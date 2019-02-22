3175b147cd8a613ae75ee5dbdc235c8f36bc78e3
Ivica Kustura <kuuste@gmail.com>
2019-02-23 00:04:01 +0100

    added transtation

--- MyProject/myapp/__init__.py
+++ MyProject/myapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_babel import Babel
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -6,10 +7,13 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7f27d8f7dd9fed2abd01e03f27fe15d8a1bd4c9c33ed8dfd05cdeec2b9abbc0f'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+babel = Babel(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 
 from myapp import routes
--- MyProject/myapp/routes.py
+++ MyProject/myapp/routes.py
@@ -4,19 +4,22 @@ import json
 import pandas
 import random
 import requests
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, session
 from myapp import app, db, bcrypt
 from myapp.forms import RegisterForm, LoginForm
 from myapp.models import User, Movies
 from flask_login import login_user, current_user, logout_user, login_required
+from myapp import babel
 
 fileName = 'myapp/idList/links.csv'
 API_key = '1446dc92'
 
-tmpData = (
-    {"Search": [{"Title": "Batman: The Killing Joke", "Year": "2016", "imdbID": "tt4853102", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMTdjZTliODYtNWExMi00NjQ1LWIzN2MtN2Q5NTg5NTk3NzliL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Batman: The Dark Knight Returns, Part 2", "Year": "2013", "imdbID": "tt2166834", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BYTEzMmE0ZDYtYWNmYi00ZWM4LWJjOTUtYTE0ZmQyYWM3ZjA0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"}, {"Title": "Batman: Mask of the Phantasm", "Year": "1993", "imdbID": "tt0106364", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BODE0YTBiMjQtNWQyZC00YTNjLWJmYjAtMWUwNzI4NGVlZTAzL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Batman: Year One", "Year": "2011", "imdbID": "tt1672723", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BNTJjMmVkZjctNjNjMS00ZmI2LTlmYWEtOWNiYmQxYjY0YWVhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Batman: The Movie", "Year": "1966", "imdbID": "tt0060153", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BODVjNjdlYTQtMWIwYy00MWIyLWI2ZmMtZWFhMTdjNjAzNGVlXkEyXkFqcGdeQXVyNTAyNDQ2NjI@._V1_SX300.jpg"},
-                {"Title": "Batman: Assault on Arkham", "Year": "2014", "imdbID": "tt3139086", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BZDU1ZGRiY2YtYmZjMi00ZDQwLWJjMWMtNzUwNDMwYjQ4ZTVhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Batman: Gotham Knight", "Year": "2008", "imdbID": "tt1117563", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BM2I0YTFjOTUtMWYzNC00ZTgyLTk2NWEtMmE3N2VlYjEwN2JlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Superman/Batman: Apocalypse", "Year": "2010", "imdbID": "tt1673430", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMjk3ODhmNjgtZjllOC00ZWZjLTkwYzQtNzc1Y2ZhMjY2ODE0XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}, {"Title": "Batman Beyond", "Year": "1999–2001", "imdbID": "tt0147746", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BYTBiZjFlZDQtZjc1MS00YzllLWE5ZTQtMmM5OTkyNjZjMWI3XkEyXkFqcGdeQXVyMTA1OTEwNjE@._V1_SX300.jpg"}, {"Title": "Batman: Arkham City", "Year": "2011", "imdbID": "tt1568322", "Type": "game", "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BOGY2NDY5NjEtNjljNy00NGU4LTljMzQtNTgxMWYxNzI5YjlhXkEyXkFqcGdeQXVyNDQwODY1NTY@._V1_SX300.jpg"}], "totalResults": "355", "Response": "True"}
-)
+@babel.localeselector
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'en')
+
 # metoda koja slucajnim odabirom bira id iz fajla koji sadrzi imdbID,
 # api ne dopusta direktno povlacenje popisa svih id-a pa je ovo bio jedini nacin
 def selectId():
@@ -32,20 +35,22 @@ def selectId():
 def root():
     IMDB_ID = selectId()
     API_link = "https://www.omdbapi.com/?i=tt"+IMDB_ID+"&apikey="+API_key
+    link =  "https://www.imdb.com/title/tt"+IMDB_ID+"/?ref_=nv_sr_1"
     API_link.replace(" ", "")
     resp = requests.get(API_link)
     movie = resp.json()
-    return render_template('home.html', title='Home', mov=movie)
+    return render_template('home.html', title='Home', mov=movie,link=link)
 
 # ruta koja prekazuje tocno odabran film
 @app.route('/<id>')
 def display(id):
     movid = id[2:]
     API_link = "https://www.omdbapi.com/?i=tt"+movid+"&apikey="+API_key
+    link =  "https://www.imdb.com/title/tt"+movid+"/?ref_=nv_sr_1"
     API_link.replace(" ", "")
     resp = requests.get(API_link)
     movie = resp.json()
-    return render_template('home.html', title='Home', mov=movie)
+    return render_template('home.html', title='Home', mov=movie, link=link)
 
 # ruta koja upravlja search rezultatima
 @app.route('/search', methods=['GET', 'POST'])
@@ -58,7 +63,7 @@ def search():
     resp = requests.get(API_link)
     movie = resp.json()
     brStr = round(int(movie.get('totalResults'))/9)
-   
+
 
     return render_template('search.html', title='Search', mov=movie, br=br, input=input, brStr=brStr)
 
@@ -101,7 +106,7 @@ def selectPage(input, br):
             resp = requests.get(API_link)
             movie = resp.json()
             brStr = round(int(movie.get('totalResults'))/9)
-            
+
             return render_template('search.html', title='Search', mov=movie, br=1, input=input, brStr=brStr)
 
 # dodaje fil iz root rute u bazu na klik botuna
